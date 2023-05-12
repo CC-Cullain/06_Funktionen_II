0e5451a5f9e8b4d7c8f9e8c38c6ee24208ce0ce7
Peter <schmidt.multimedia@gmail.com>
2023-05-12 10:43:53 +0200

    module: division a / b / 1. Iteration

--- script_01.js
+++ script_01.js
@@ -11,10 +11,28 @@
 4. Ausgabe in Konsole : check!
 */
 
+
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(3,0));
+function divide(a,b) {
+
+	if (b != 0) {
+		return a/b;
+	} else {
+		return "Teilen durch 0 nicht möglich";
+	}
+
+	
+}
+
+
 // module: multiplication a * b |  test:
-output(multiply(3,2));
-output(multiply(3,-2));
-output(multiply(3,0));
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
 function multiply(a,b) {
 	return a*b;
 }
