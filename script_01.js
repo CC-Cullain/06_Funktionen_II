cd523adc226a2d608a9dec9728942e4652bcebc7
Cullain <lukas.gedrat@gmx.net>
2023-05-12 10:23:39 +0200

    Rechenarten

--- script_01.js
+++ script_01.js
@@ -11,13 +11,60 @@
 4. Ausgabe in Konsole : 
 */
 
+// Dateieingabe
 
+function input(a,b) {
+	
 
+}
 
+// Dateikonvertierung
+
+function convert(a,b) {
+
+	return parseInt(a), parseInt(b);
+}
+
+// Auswahl Rechenart
+
+function name(params) {
+	
+}
+
+//  module: addition a+b
+// output(add(2,2));
+
+function add(a,b) {
+	
+	return a + b;
+}
+
+//  module: subtraction a+b
+// output(sub(6,3));
+
+function subtract(a,b) {
+	
+	return a - b;
+}
+
+//  module: multiplication a+b
+// output(mult(6,2));
+
+function multiply(a,b) {
+	
+	return a * b;
+}
+
+//  module: division a+b
+// output(div(6,2));
+
+function divide(a,b) {
+	
+	return a / b;
+}
 
 // module: output | test:
-// output("hello");
-// output(2);
+
 function output(outputData) {
 	console.log(outputData);
 }
